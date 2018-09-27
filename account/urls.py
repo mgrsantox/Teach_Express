bf75047863eab4c90d5f640372c14722be93d3cf
Santosh Rana <pysantosh.np@gmail.com>
2018-09-27 15:50:00 +0545

    testing commit

---
+++ account/urls.py
@@ -0,0 +1,18 @@
+from os import name
+
+from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from .views import dashboard, user_login
+
+
+app_name = 'account'
+
+urlpatterns = [
+    # path('login/', user_login, name='login')
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/dashboard/', dashboard, name='dashboard'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(),name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
